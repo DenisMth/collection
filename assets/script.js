b25e1f651a720f5b499144204eace39b090a5061
Denis Mathieu <denismathieu1203@gmail.com>
2023-12-22 10:09:44 +0100

    test

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-import {collection as collection} from "./collection.js";
+import collection from "./collection.js";
 
 const newButtonBlock = document.createElement("div");
 newButtonBlock.className = "buttonBlock";
